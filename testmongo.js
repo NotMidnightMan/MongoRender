17ad2e39650160604c711d4632bb1a2daa4d05c0
Jake Barrios <94154490+NotMidnightMan@users.noreply.github.com>
2025-04-08 11:19:23 -0500

    Update testmongo.js

--- testmongo.js
+++ testmongo.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://<jakeadmin>:<admin29!>@mongocluster.z6vdfo7.mongodb.net/?retryWrites=true&w=majority&appName=MongoCluster";
+const uri = "mongodb+srv://jakeadmin:admin29!@mongocluster.z6vdfo7.mongodb.net/?retryWrites=true&w=majority&appName=MongoCluster";
 
 // --- This is the standard stuff to get it to work on the browser
 const express = require('express');
@@ -33,8 +33,8 @@ console.log("Looking for: " + searchKey);
 
 async function run() {
   try {
-    const database = client.db('ckmdb');
-    const parts = database.collection('cmps415');
+    const database = client.db('MongoRenderDB');
+    const parts = database.collection('MongoRender');
 
     // Hardwired Query for a part that has partID '12345'
     // const query = { partID: '12345' };
