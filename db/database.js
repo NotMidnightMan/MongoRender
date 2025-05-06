ff19017a218a9c7f7bd892779b4f2037d96a1da8
Jake Barrios <94154490+NotMidnightMan@users.noreply.github.com>
2025-05-06 18:10:46 +0000

    database work

--- db/database.js
+++ db/database.js
@@ -1,9 +1,22 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected successfully'))
-.catch(err => console.error('MongoDB connection error:', err));
+const connectDB = async () => {
+  if (mongoose.connection.readyState === 0) {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Database connected');
+
+    // Log the database name
+    console.log('Connected to database:', mongoose.connection.name);
+
+    // Log the collections in the database
+    const collections = await mongoose.connection.db.listCollections().toArray();
+    console.log('Collections in the database:', collections.map(c => c.name));
+  }
+};
+
+connectDB();
+
+module.exports = mongoose; // Export the Mongoose instance
